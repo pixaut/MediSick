e57b6e0dc640b69c782d1ba191007895f855e072
epkorq <vitals07@bk.ru>
2023-08-24 12:31:45 +0300

    ыыы

--- input_maker.py
+++ input_maker.py
@@ -1,22 +1,31 @@
 import cv2
 import os.path
+import os
 
-f = open('lib.txt', 'w')
+directory = 'assets\\images' # папка с картинками
+files = os.listdir(directory) # массив с файлами
+files_size = len(files) # кол-во картинок с буквами
 
-for k in range(25):
-    path = 'assets\\images\\'+'letter'+str(k)+'.png'
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    w = img.shape[0]
-    h = img.shape[1]
 
-    for i in range(w):
-        for j in range(h):
-            a = img[i, j]
-            ans = round((255 - a) / 255, 1)
-            f.write(str(ans))
-            f.write(' ')
-        f.write('\n')
-    f.write('\n')
-f.close()
+f = open('assets\\lib.txt', 'w') # открываем файл для записи
+
+for k in range(files_size): # проходимся по каждой букве
+
+    path = 'assets\\images\\'+'letter'+str(chr(ord('a') + k))+'.png' # путь к букве
+    if (os.path.isfile(path) == True):
+
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # записываем картинку в переменную
+        w = img.shape[0] # ширина
+        h = img.shape[1] # высота
+
+        for i in range(w):
+            for j in range(h):
+                a = img[i, j] # параметр пикселя
+                ans = round((255 - a) / 255, 1) # 1 - полностью черный, 0 - полностью белый
+                f.write(str(ans)) # записываем параметр пикселя в файл
+                f.write(' ')
+            f.write('\n')  # разделители
+        f.write(str(chr(ord('a') + k)))
+f.close() # закрыть файл
 
 

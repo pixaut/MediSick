a68b54bc878f7ce7e801f3895236b40d16a6f995
pixaut <skazov.andrey.by@gmail.com>
2023-08-30 16:05:17 +0300

    Andrey

--- input_maker.py
+++ input_maker.py
@@ -2,25 +2,22 @@ import cv2
 import os.path
 import os
 
-directory = 'assets\\images' # папка с картинками
-files = os.listdir(directory) # массив с файлами
-files_size = len(files) # кол-во картинок с буквами
-
-
 f = open('assets\\lib.txt', 'w') # открываем файл для записи
 
 for k in range(26): # проходимся по каждой букве
 
     l = 0
     
-    path = directory+'\\letter'+str(chr(ord('a') + k)) # путь к букве
-    while(os.path.isfile(path) == True):
+    path = 'assets\\images\\letter'+str(chr(ord('a') + k)) # путь к букве
 
-        path2 = path + str(l) + '.png'
+    path2 = path +'0.png'
+
+    while(os.path.exists(path2)):
+
         l = l + 1
-        
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # записываем картинку в переменную
-        w,h = 64# ширина высота
+
+        img = cv2.imread(path2, cv2.IMREAD_GRAYSCALE) # записываем картинку в переменную
+        w,h = 64,64# ширина высота
 
         for i in range(w):
             for j in range(h):
@@ -30,6 +27,9 @@ for k in range(26): # проходимся по каждой букве
                 f.write(' ')
             f.write('\n')  # разделители
         f.write(str(chr(ord('a') + k)))
+        f.write('\n')
+        path2 = path + str(l) + '.png'
+
 f.close() # закрыть файл
 
 

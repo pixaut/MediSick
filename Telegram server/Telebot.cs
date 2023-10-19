702d6d45588353e6bc64bed8915a0fab388276c9
DrgNBoN <95080712+DrgNBoN@users.noreply.github.com>
2023-10-19 22:17:24 +0300

    save

--- Telegram server/Telebot.cs
+++ Telegram server/Telebot.cs
@@ -212,10 +212,6 @@ namespace Program
             Dictionarypreparer.DatabaseDictSaverToJSON(database, settings!.pathdatabasejson);
         }
 
-
-
-
-
         private static Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             var ErrorMessage = exception switch
@@ -224,12 +220,8 @@ namespace Program
                     => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
                 _ => exception.ToString()
             };
-
             Console.WriteLine(ErrorMessage);
             return Task.CompletedTask;
         }
-
-
-
     }
 }

e81c04d6ccc042332dd127d378bfb2ea6a2bee1e
DrgNBoN <95080712+DrgNBoN@users.noreply.github.com>
2023-10-23 21:46:43 +0300

    json's added

---
+++ Telegram server/SymptomsList.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class First2
+    {
+        public Second2[] Symptoms { get; set; }
+
+    }
+    public class Second2
+    {
+        public string Сategory { get; set; }
+        public string List { get; set; }
+    }
+}

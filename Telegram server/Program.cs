34c8412ff8497aefe506ff76c36f1ae81903a6a5
DrgNBoN <95080712+DrgNBoN@users.noreply.github.com>
2023-10-23 21:46:39 +0300

    sample with "hi"

--- Telegram server/Program.cs
+++ Telegram server/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("6497653264:AAFruV6L5WFBy3DudYPt-WyhQgWsXJQlFqY");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botclient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text is not null)
+            {
+                if (message.Text.ToLower().Contains("hallo"))
+                {
+                    await botclient.SendTextMessageAsync(message.Chat.Id, "hi");
+                    return;
+                }
+
+            }
+
+
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

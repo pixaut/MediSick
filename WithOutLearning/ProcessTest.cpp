b595e454bacb02d0b35fc7b5fd7138a3a2a39bc4
pixaut <skazov.andrey.by@gmail.com>
2023-10-24 09:10:56 +0300

    fix update

--- WithOutLearning/ProcessTest.cpp
+++ WithOutLearning/ProcessTest.cpp
@@ -1,49 +1,50 @@
 #include "..\Library\NeuronNetwork.h"
 #include <fstream>
 
-
 int main(){
 
-    std::ifstream fin;
+   std::ifstream fin;
 
-    char UserId[256];                                                        // userid
-    int n,c;                                                                // quantity of simphtones, c - number of symphtone
-                                                        // 50 - number of input values
-
-    int layers;
-
-    fin.open("..\\NetworkDescription\\NetworkSize.txt");                    //
-    fin >> layers;                                                          //
-    int* size = new int[layers];                                            //
-    for(int i = 0;i < layers;i++){                                          //          Descript Network
-        fin >> size[i];                                                     //
-    }                                                                       //
-    fin.close();                                                            //
-
-    double input[size[0]];   
-
-    NeuronNetwork nn(layers,size);                                          //                                            
-    nn.LoadNetwork("..\\NetworkDescription\\Network.txt");                  //
-
-    fin.open("input.txt");                                                  //
-    
-    std::fill(input,input+size[0],0.0);
-
-    fin >> UserId;                                                          //
-    fin >> n;                                                               //
-    for(int i = 0;i < n;i++){                                               //
-        fin >> c;                                                           //  Input simphtones
-        input[c-1] = 1.0;                                                   //
-    }                                                                       //
-    fin.close();                                                            //
-    nn.SetInput(input);
+   char UserId[256];                                                        
+   int n,c,layers;                                                                
 
 
+    /*Set network size*/                                   
+   fin.open("..\\NetworkDescription\\NetworkSize.txt");                    
+   fin >> layers;                                                          
+   int* size = new int[layers];                                            
+   for(int i = 0;i < layers;i++){                                          
+       fin >> size[i];                                                     
+   }                                                                       
+   fin.close();                                                            
 
-    nn.ForwardFeed();                                                       //
-    std::ofstream fout("output.txt");                                       //
-    fout << UserId << ' ' << nn.Predict()+1;                                  // do output
-    fout.close();                                                           //
+   double input[size[0]];   
 
-    return 0;
+
+   /*Set network weights*/
+   NeuronNetwork nn(layers,size);                                                                                    
+   nn.LoadNetwork("..\\NetworkDescription\\Network.txt");                  
+
+   fin.open("..\\Telegram server\\bin\\Debug\\net7.0\\Inputuser\\input.txt");                                                  
+   
+   std::fill(input,input+size[0],0.0);
+
+    /*Input question*/
+
+   fin >> UserId;                                                          
+   fin >> n;                                                               
+   for(int i = 0;i < n;i++){                                               
+       fin >> c;                                                           
+       input[c-1] = 1.0;                                                   
+   }                                                                       
+   fin.close();                                                            
+   nn.SetInput(input);
+
+    /*Answer question*/
+   nn.ForwardFeed();                                                       
+   std::ofstream fout("..\\Telegram server\\bin\\Debug\\net7.0\\Outputuser\\output.txt");                                       
+   fout << UserId << ' ' << nn.Predict()+1;       
+   fout.close();                                                           
+
+   return 0;
 }

3dbf7acbf109e0e6cd08464fb1a3a12b4856d47d
DrgNBoN <95080712+DrgNBoN@users.noreply.github.com>
2023-11-24 03:29:38 +0300

    Synchronization,many bugs fixed,semi-final version

---
+++ Telegram server/DrugsParser.cs
@@ -0,0 +1,79 @@
+﻿namespace Program
+{
+    //Parser drugs and pharms part:
+    public class DrugsParser
+    {
+        public static HtmlDocument doc = new HtmlDocument();
+
+        //Search drugs in current city function:
+        public static async Task parsedrugslist(string drugsearchname, int index)
+        {
+            database[userid].lastdrugslist.Clear();
+            DatabaseDictSaverToJSON(database, settings!.pathdatabasejson);
+            List<DrugSpecs> drugslist = new List<DrugSpecs>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://tabletka.by/search?request={drugsearchname}&region={index}");
+                    HttpResponseMessage response = await httpClient.SendAsync(request);
+                    doc.LoadHtml(await response.Content.ReadAsStringAsync());
+                    HtmlNodeCollection drugname = doc.DocumentNode.SelectNodes("//div[@class='table-wrap']//td[@class='name tooltip-info']//div[@class='tooltip-info-header']/a");
+                    HtmlNodeCollection drugform = doc.DocumentNode.SelectNodes("//div[@class='table-wrap']//td[@class='form tooltip-info']//div[@class='tooltip-info-header']/a");
+                    HtmlNodeCollection drugproducer = doc.DocumentNode.SelectNodes("//div[@class='table-wrap']//td[@class='produce tooltip-info']//div[@class='tooltip-info-header']/a");
+                    HtmlNodeCollection drugprice = doc.DocumentNode.SelectNodes("//div[@class='table-wrap']//td[@class='price']//span[@class='price-value']");
+                    HtmlNodeCollection numberofpharmacies = doc.DocumentNode.SelectNodes("//div[@class='table-wrap']//td[@class='price']//span[@class='capture']/a");
+                    for (int i = 0; i < Math.Min(5, drugname.Count); i++)
+                    {
+                        int.TryParse(string.Join("", numberofpharmacies[i].InnerText.Where(c => char.IsDigit(c))), out int pharmaciescount);
+                        DrugSpecs drugspec = new DrugSpecs()
+                        {
+                            Drugname = drugname[i].InnerText,
+                            Drugform = drugform[i].InnerText,
+                            Drugproducer = drugproducer[i].InnerText.Trim(),
+                            Drugprice = drugprice[i].InnerText,
+                            Link = drugname[i].GetAttributeValue("href", ""),
+                            Numberofpharmacies = pharmaciescount
+                        };
+                        drugslist.Add(drugspec);
+                    }
+                    database[userid].lastdrugslist = drugslist;
+                }
+                catch { }
+            }
+        }
+
+        //Search pharms for drugs in current city function:
+        public static async Task parsedrugsincity(string link)
+        {
+            List<DrugInSityInfo> pharmlist = new List<DrugInSityInfo>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://tabletka.by{link}");
+                    HttpResponseMessage response = await httpClient.SendAsync(request);
+                    doc.LoadHtml(await response.Content.ReadAsStringAsync());
+                    HtmlNodeCollection pharmname = doc.DocumentNode.SelectNodes("//div[@class='table-wrap reload result-table']//td[@class='pharm-name']//div[@class='tooltip-info-header']//a");
+                    HtmlNodeCollection address = doc.DocumentNode.SelectNodes("//div[@class='table-wrap reload result-table']//td[@class='address tooltip-info']//div[@class='tooltip-info-header']/div[@class='text-wrap']/span");
+                    HtmlNodeCollection phonenumber = doc.DocumentNode.SelectNodes("//div[@class='table-wrap reload result-table']//td[@class='phone tooltip-info']//div[@class='tooltip-info-header']/div[@class='text-wrap']/a");
+                    HtmlNodeCollection cost = doc.DocumentNode.SelectNodes("//div[@class='table-wrap reload result-table']//td[@class='price tooltip-info']//div[@class='tooltip-info-header']/div[@class='text-wrap']/span");
+                    for (int i = 0; i < Math.Min(5, pharmname.Count); ++i)
+                    {
+                        DrugInSityInfo pharminfo = new DrugInSityInfo()
+                        {
+                            Pharmname = pharmname[i].InnerText,
+                            Address = address[i].InnerText.Trim(),
+                            PhoneNumber = phonenumber[i].InnerText,
+                            Cost = cost[i].InnerText.Trim(),
+                        };
+                        pharmlist.Add(pharminfo);
+                    }
+                }
+                catch { }
+            }
+            database[userid].lastpharmlist = pharmlist;
+            DatabaseDictSaverToJSON(database, settings!.pathdatabasejson);
+        }
+    }
+}
---
+++ Telegram server/YandexMapParser.cs
@@ -0,0 +1,65 @@
+﻿namespace Program
+{
+    //Parser YandexMap part:
+    public class YandexMapParser
+    {
+        //Searchorganizations YandexMap function:
+        public static string searchorganizations(string organization, (double, double) coordinates)
+        {
+            string outsting = "";
+            double bias = settings!.kilometerstolerance! / 111.134861111;
+
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Uri address = new Uri($"https://search-maps.yandex.ru/v1/?text={organization}&bbox={coordinates.Item2},{coordinates.Item1}~{coordinates.Item2 + bias},{coordinates.Item1 + bias}&type=biz&lang={database[userid].language + "_RU"}&results={settings!.searchresultsarea!}&apikey={settings!.yandexmaptoken!}");
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    database[userid]!.listofrecentsearchedplaces!.Clear();
+                    client.Encoding = Encoding.UTF8;
+                    string request = client.DownloadString(address);
+                    Rootobject1 answer = JsonConvert.DeserializeObject<Rootobject1>(request)!;
+                    outsting += botword["longlinetext"];
+                    foreach (var feature in answer!.features)
+                    {
+                        database[userid]!.listofrecentsearchedplaces!.Add((feature.geometry.coordinates[1], feature.geometry.coordinates[0], feature.properties.CompanyMetaData.name, feature.properties.CompanyMetaData.address)!);
+                        if (feature.properties.CompanyMetaData.name != null) outsting += $"➡️{organization}: <b>\"{feature.properties.CompanyMetaData.name}\"</b>\n";
+                        if (feature.properties.CompanyMetaData.address != null) outsting += $"🗺️<b>{botword["addresstext"]}</b> <i>{feature.properties.CompanyMetaData.address}</i> \n📞<b>{botword["phonenumberstext"]}</b>\n";
+                        if (feature.properties.CompanyMetaData.Phones != null) foreach (var formatted in feature.properties.CompanyMetaData.Phones) outsting += $"          <i>{formatted.formatted}</i>\n";
+                        if (feature.properties.CompanyMetaData.Hours.text != null) outsting += $"📅<b>{botword["operatingscheduletext"]}</b> <i>{feature.properties.CompanyMetaData.Hours.text}</i>\n";
+                        if (feature.properties.CompanyMetaData.url != null) outsting += $"🌐<b>{botword["websitetext"]}</b> {feature.properties.CompanyMetaData.url}\n";
+                        outsting += botword["longlinetext"];
+                    }
+                }
+                catch { }
+            }
+            if (outsting == "" || outsting == null) outsting = botword["sorrynoinfotext"];
+            return outsting;
+        }
+
+        //Search current city from YandexMap function:
+        public static void determineaddress((double, double) coordinates)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Uri address = new Uri($"https://geocode-maps.yandex.ru/1.x/?apikey={settings!.yandexmaptoken2!}&geocode={coordinates.Item2},{coordinates.Item1}&format=json");
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    database[userid]!.listofrecentsearchedplaces!.Clear();
+                    client.Encoding = Encoding.UTF8;
+                    string request = client.DownloadString(address);
+                    Rootobject2 answer = JsonConvert.DeserializeObject<Rootobject2>(request)!;
+                    database[userid].city = answer.response.GeoObjectCollection.featureMember[0].GeoObject.description.Substring(0, answer.response.GeoObjectCollection.featureMember[0].GeoObject.description.IndexOf(','));
+                    DatabaseDictSaverToJSON(database, settings!.pathdatabasejson);
+                }
+                catch { }
+            }
+        }
+    }
+}
